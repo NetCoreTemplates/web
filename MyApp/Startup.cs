c91c09925954ee939c1735685ca770ff998dcc12
Demis Bellot <demis.bellot@gmail.com>
2017-11-14 01:33:16 -0500

    Change to use the new IConfiguration NetCoreAppSettings adapter

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Funq;
 using ServiceStack;
+using ServiceStack.Configuration;
 using MyApp.ServiceInterface;
 
 namespace MyApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,7 +28,10 @@ namespace MyApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseServiceStack(new AppHost());
+            app.UseServiceStack(new AppHost
+            {
+                AppSettings = new NetCoreAppSettings(Configuration)
+            });
         }
     }
 
@@ -36,7 +44,8 @@ namespace MyApp
         {
             SetConfig(new HostConfig
             {
-                DefaultRedirectPath = "/metadata"
+                DefaultRedirectPath = "/metadata",
+                DebugMode = AppSettings.Get(nameof(HostConfig.DebugMode), false)
             });
         }
     }
