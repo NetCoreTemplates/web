6d1979153c68a705020a46946e7f89f7cd5c11b7
Demis Bellot <demis.bellot@gmail.com>
2019-08-06 22:11:08 -0400

    Update Startup.cs

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -9,14 +9,13 @@ using MyApp.ServiceInterface;
 
 namespace MyApp
 {
-    public class Startup
+    public class Startup : ModularStartup
     {
-        public IConfiguration Configuration { get; }
-        public Startup(IConfiguration configuration) => Configuration = configuration;
+        public Startup(IConfiguration configuration) : base(configuration){}
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public new void ConfigureServices(IServiceCollection services)
         {
         }
 
