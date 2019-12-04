542735221d31615f386a9719adfd8600ff3083ad
Demis Bellot <demis.bellot@gmail.com>
2019-12-03 23:48:21 -0500

    Upgrade to .NET Core 3.1 LTS

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Funq;
@@ -11,8 +12,6 @@ namespace MyApp
 {
     public class Startup : ModularStartup
     {
-        public Startup(IConfiguration configuration) : base(configuration){}
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public new void ConfigureServices(IServiceCollection services)
@@ -20,7 +19,7 @@ namespace MyApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # web
 
-.NET Core 2.1 Empty Web Template
+.NET Core 3.1 Empty Web Template
 
 [![](https://raw.githubusercontent.com/ServiceStack/Assets/master/csharp-templates/web.png)](http://web.web-templates.io/)
 
