2aabbaf86470dae79525e4397b145ef446ebac9f
Demis Bellot <demis.bellot@gmail.com>
2024-02-07 18:16:53 +0800

    Remove Swagger from empty templates (can now add with x mix openapi3)

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,23 +3,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
-services.AddServiceStack(typeof(MyServices).Assembly, c =>
-{
-    c.AddSwagger();
-});
-
-services.AddEndpointsApiExplorer();
-services.AddSwaggerGen();
+services.AddServiceStack(typeof(MyServices).Assembly);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
